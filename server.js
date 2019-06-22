913671700316b08411c2ac7b94e9ba405e00e077
Simran Gill <simgill89@gmail.com>
2019-06-21 20:18:28 -0700

    inital

---
+++ server.js
@@ -0,0 +1,123 @@
+const env =  require('dotenv').config().parsed;
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const app = express();
+app.use(bodyParser.json());
+const path = require('path');
+const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || env.DATABASE_URL;
+const PORT =  (process.env.PORT || 9000)
+
+
+
+console.log("++++ENV",env)
+
+
+app.use('/static', express.static(path.join(__dirname, './public')))
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let server;
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
